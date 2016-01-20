cbe1a40231fb749898ada04774c03990c8d841ec
Brian Tingle <brian.tingle.cdlib.org@gmail.com>
2016-01-20 08:54:14 -0800

    extent reports

--- nuxeo.py
+++ nuxeo.py
@@ -5,16 +5,18 @@ import sys
 import argparse
 import os
 from pynux import utils
+import xlsxwriter
+import time
 
 from pprint import pprint as pp
 
 
 def main(argv=None):
 
-    parser = argparse.ArgumentParser(description='nxql via REST API')
+    parser = argparse.ArgumentParser(description='extent stats via Nuxeo REST API')
     parser.add_argument('nxql', nargs=1, help="nxql query")
-    parser.add_argument('--outdir', 
-        help="directory to hold application/json+nxentity .json files")
+    parser.add_argument('-x', '--xlsx', 
+        help="xlsx")
     utils.get_common_options(parser)
     if argv is None:
         argv = parser.parse_args()
@@ -22,29 +24,56 @@ def main(argv=None):
     nx = utils.Nuxeo(rcfile=argv.rcfile, loglevel=argv.loglevel.upper())
 
     documents = nx.nxql('select * from Document where ecm:path startswith"{0}"'.format(argv.nxql[0]))
-    if argv.outdir:
-        # Expand user- and relative-paths
-        outdir = os.path.abspath(os.path.expanduser(argv.outdir))
-        nx.copy_metadata_to_local(documents, outdir)
-    else:
-        for document in documents:
-            for blob in blob_from_doc(document):
-                if blob:
-                    print(format_blob(blob).encode('utf-8'))
 
+    # open the workbook
+    workbook = xlsxwriter.Workbook(argv.xlsx)
+    # set up a worksheet for each page
 
-def format_blob(blob):
-    return u'\t'.join([
-        blob['uid'],
-        blob['path'],
-        blob['xpath'],
-        blob['name'],
-        blob['data'],
-        u'{}:{}'.format(blob['digestAlgorithm'],blob['digest']),
-        blob['length'],
-        blob['mime-type'],
-    ])
+    header_format = workbook.add_format({'bold': True, })
+    number_format = workbook.add_format()
+    number_format.set_num_format('#,##0')
     
+    campus = workbook.add_worksheet('Campus')
+    # headers
+    campus.write(0, 0, 'row', header_format)
+    campus.write(0, 1, 'uid', header_format)
+    campus.write(0, 2, 'path', header_format)
+    campus.write(0, 3, 'xpath', header_format)
+    campus.write(0, 4, 'name', header_format)
+    campus.write(0, 5, 'data-url', header_format)
+    campus.write(0, 6, 'md5', header_format)
+    campus.write(0, 7, 'bytes', header_format)
+    campus.write(0, 8, 'mime-type', header_format)
+    # width
+    campus.set_column(0, 0, 3, )
+    campus.set_column(1, 1, 10, )
+    campus.set_column(2, 2, 40, )
+    campus.set_column(3, 3, 5, )
+    campus.set_column(4, 4, 20, )
+    campus.set_column(5, 5, 5, )
+    campus.set_column(6, 6, 20, )
+    campus.set_column(7, 7, 10, )
+    campus.set_column(8, 8, 10, )
+
+    row = 1 
+    for document in documents:
+        for blob in blob_from_doc(document):
+            if blob:
+                campus.write(row, 0, row, number_format)
+                campus.write(row, 1, blob['uid'],)
+                campus.write(row, 2, blob['path'],)
+                campus.write(row, 3, blob['xpath'],)
+                campus.write(row, 4, blob['name'],)
+                campus.write(row, 5, blob['data'],)
+                campus.write(row, 6, blob['digest'],)
+                campus.write(row, 7, int(blob['length']), number_format)
+                campus.write(row, 8, blob['mime-type'],)
+                row = row + 1
+
+    campus.write_formula(row, 7, '=SUM(H2:H{})'.format(row))
+    run = '{}'.format(time.ctime())
+    campus.write(row, 2, run)
+
 
 def blob_from_doc(document):
     blobs = []
