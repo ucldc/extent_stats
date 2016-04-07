888aae217b5570810526051c2eeef0d16ffaf7e3
Brian Tingle <brian.tingle@ucop.edu>
2016-04-07 18:00:18 +0000

    remove debug print

--- nuxeo.py
+++ nuxeo.py
@@ -161,7 +161,6 @@ def blob_from_doc(document):
         blobs.append(main_file)
     if 'files:files' in document['properties']:
         for idx, blob in enumerate(document['properties']['files:files']):
-            pp(blob)
             if blob['file']:
                 blob['file']['xpath'] = 'files:files/item[{0}]/file'.format(idx + 1)
                 blob['file']['uid'] = document['uid']
